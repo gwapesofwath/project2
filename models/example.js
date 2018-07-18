89d4891dca30690df9977625ba410d8ef15d344a
gwapesofwath <gwapesofwath@gmail.com>
2018-07-18 11:46:56 -0500

    tiny push

--- models/example.js
+++ models/example.js
@@ -1,7 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Dream = sequelize.define("Dream", {
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    status: DataTypes.BOOLEAN,
+    duration: DataTypes.INTEGER
+
   });
-  return Example;
+  return Dream;
 };
